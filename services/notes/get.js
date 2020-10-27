c49d269589702ef5cf88a62365edf87aa9823276
mmuzio <mmuzio@hmc.edu>
2020-10-27 11:51:23 -0400

    set timeout to simulate error

--- services/notes/get.js
+++ services/notes/get.js
@@ -2,17 +2,21 @@ import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-    const params = {
-        TableName: process.env.tableName,
-        Key: {
-            userId: event.requestContext.identity.cognitoIdentityId,
-            // noteId: event.pathParameters.id
-        }
-    };
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id,
+    },
+  };
 
-    const result = await dynamoDb.get(params);
-    if (!result.Item) {
-        throw new Error("Item not found.");
-    }
-    return result.Item;
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Set a timeout
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  return result.Item;
 });
